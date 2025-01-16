445bcc9ffb0e3fca31c19db08eda2eab51f91652
r2ditto <ryan.ebro@yahoo.com>
2025-01-15 23:01:56 -0500

    install montserrat google font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
+import { Montserrat } from "next/font/google";
 import type { AppProps } from "next/app";
+
 import "../styles/globals.css";
 
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-montserrat",
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${montserrat.variable} ${montserrat.className}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
