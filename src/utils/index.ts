e5a2cc671fd5b7b7242e3d9ec69e6e06fb6bef06
r2ditto <ryan.ebro@yahoo.com>
2025-01-16 01:00:41 -0500

    add getWeatherImage utility function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,3 +7,27 @@ export const getCurrentDate = () => {
     month: "long",
   });
 };
+
+export const getWeatherImage = (weatherCondition: string | undefined) => {
+  if (!weatherCondition) return "/clouds.png";
+
+  const condition = weatherCondition.toLowerCase();
+
+  switch (condition) {
+    case "clear":
+      return "/clear.png";
+    case "rain":
+    case "drizzle":
+      return "/rain.png";
+    case "thunderstorm":
+      return "/thunder.png";
+    case "snow":
+      return "/snow.png";
+    case "mist":
+    case "fog":
+    case "haze":
+      return "/haze.png";
+    default:
+      return "/clouds.png";
+  }
+};
